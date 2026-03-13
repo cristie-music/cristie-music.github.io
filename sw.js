79257377ba7517f66c1a465acf2d7ac734699c87
Cristie <cristie@dev.null>
2026-03-13 16:41:28 +0300

    add first player version

---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'cristie-player-v1';
+// Список файлов для кэширования 
+const ASSETS = [
+  'player.html',
+  'manifest.json',
+  'background.jpg',
+  'artist2.png',
+  'index.html'
+];
+
+// Установка сервис-воркера и кэширование ресурсов
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+// Активация и очистка старого кэша
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Обработка сетевых запросов
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
